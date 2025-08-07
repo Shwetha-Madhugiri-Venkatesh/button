2fb87946e3fb0e3ad946bc7d6524abfc5fa80c9c
Shwetha <Shwetha.Venkatesh@itorizon.com>
2025-08-07 10:58:36 +0530

    end

--- src/app/comp2/comp2.component.ts
+++ src/app/comp2/comp2.component.ts
@@ -4,13 +4,21 @@ import { Comp1Component } from '../comp1/comp1.component';
 
 @Component({
   selector: 'app-comp2',
-  templateUrl: './comp2.component.html',
+  //templateUrl: './comp2.component.html',
+   template: `
+    <button (click)="check()">Check for Update</button>
+    <p>{{ data }}</p>
+  `,
   styleUrls: ['./comp2.component.css'],
 })
 export class Comp2Component {
+  data
   constructor(public service:name_emit){}
   update_name:string=this.service.name;
 
+  check() {
+    this.data = this.service.name;
+  }
   update_name_change(){
     this.update_name=this.service.name;
   }
